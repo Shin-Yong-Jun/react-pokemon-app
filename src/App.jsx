61d3bca515c30901a6b0f95992995299517f5097
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-10-24 13:01:24 +0900

    포케api 연결 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
+import { useState } from "react";
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0);
+
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetchPokeData();
+  },[]);
+
+const url = "https://pokeapi.co/api/v2/pokemon/?limit=1008&offset=0";
+
+  const fetchPokeData = async() => {
+    try {
+      const response = await axios.get(url)
+      console.log(response.data.results)
+      setPokemons(response.data.results)
+    } catch(error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <argicle className="pt-6">
+      <header className="flex flex-col justify-content items-center overflow-auto z-0">
+        {/* {Input Form 부분} */}
+        input form
+      </header>
+      <section className="pt-6 flex flex-col justify-content items-center overflow-auto z-0">
+        <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
+          {pokemons.length > 0 ? (
+            pokemons.map(({url, name}, index) =>(
+              <div key={index}>
+                {name}
+              </div>
+            ))
+          ) : (
+            <h2 className="font-medium text-lg text-slate-900 mb-1">
+              포켓몬이 없습니다.
+            </h2>
+          )}
+        </div>
+      </section>
+    </argicle>
+
   );
 }
 
