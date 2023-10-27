3d7f18ba3acc1bbf2aa49b67644d096c02ba23f5
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-10-27 17:13:01 +0900

    cjs 방식으로 변경완료

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-import tailwindScrollbar from 'tailwind-scrollbar';
 
-export default {
+module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     './index.html',
@@ -35,5 +34,5 @@ export default {
       }
     },
   },
-  plugins: [tailwindScrollbar],
+  plugins: [require('tailwind-scrollbar')],
 }
