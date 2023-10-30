37f7118e53e145e9d542739fd14134787348d97c
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-10-31 00:50:54 +0900

    AutoComplete 연습완료 및 Test2.jsx 삭제

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
 
-const AutoComplete = ({allPokemons, setDisplayedPokemons}) => {
-
+const AutoComplete = ({ allPokemons, setDisplayedPokemons }) => {
   const [searchTerm, setSearchTerm] = useState("");
 
-
-  const filterNames = (input) => {
-    const value = input.toLowerCase();
+  // 이게 실제 찾아주는 역할! include!
+  const filterNames = (Input) => {
+    const value = Input.toLowerCase();
     return value ? allPokemons.filter((e) => e.name.includes(value)) : [];
-  }
+  };
 
-
-  const handleSubmit = (e) => {
+  // 검색버튼을 누를때 실제 입력값이 포함된 데이터를 찾아주는 기능구현
+  function handleSubmit(e) {
     e.preventDefault();
 
     let text = searchTerm.trim();
@@ -19,16 +18,17 @@ const AutoComplete = ({allPokemons, setDisplayedPokemons}) => {
     setSearchTerm("");
   }
 
+  // 검색하는 것에 정확히 일치하는 포켓몬 이름이 있으면 autoComplete 영역 없애기
   const checkEqualName = (input) => {
     const filteredArray = filterNames(input);
-
+    // 배열로 구성되어있고 0번째 값에 name이 들어가있어서 그걸 비교해야함.
     return filteredArray[0]?.name === input ? [] : filteredArray;
-  }
-
+  };
 
   return (
     <div className="relative z-50">
       <form
+        // 그 다음 애써 만든 함수기능을 onSubmit속성으로 연결
         onSubmit={handleSubmit}
         className="relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto"
       >
@@ -47,38 +47,30 @@ const AutoComplete = ({allPokemons, setDisplayedPokemons}) => {
         </button>
       </form>
       {checkEqualName(searchTerm).length > 0 && (
+        // 전체를 감싸는 부분
         <div
-          className={
-            `w-full flex bottom-0 h-0 flex-col absolute justify-center items-center translate-y-2`
-          }
+          className={`w-full flex bottom-0 h-0 flex-col absolute justify-center items-center translate-y-2`}
         >
+          {/* 세모 화살표 부분 */}
           <div
-            className={
-              `w-0 h-0 bottom-0 border-x-transparent border-x-8 border-b-[8px] border-gray-700 -translate-y-1/2`
-            }
-          >
-          </div>
+            className={`w-0 h-0 bottom-0 border-x-transparent border-x-8 border-b-[8px] border-gray-700 -translate-y-1/2`}
+          ></div>
 
           <ul
-            className={
-              `w-40 max-h-[134px] py-1 bg-gray-700 rounded-lg absolute top-0 overflow-auto scrollbar-none`
-              }
+            className={`w-40 max-h-[134px] py-1 bg-gray-700 rounded-lg absolute top-0 overflow-auto scrollbar-none`}
           >
-            {checkEqualName(searchTerm).map((e,i)=>(
+            {checkEqualName(searchTerm).map((e, i) => (
               <li key={`button-${i}`}>
                 <button
+                  // autoComplete으로 나열된 문자를 선택하면 해당 문자가 검색태그에 입력되게.
                   onClick={() => setSearchTerm(e.name)}
-                  className={
-                    `text-base w-full hover:bg-gray-600 p-[2px] text-gray-100`
-                    }>
+                  className={`text-base w-full hover:bg-gray-600 p-[2px] text-gray-100`}
+                >
                   {e.name}
                 </button>
               </li>
             ))}
-
-
           </ul>
-
         </div>
       )}
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Test2 from './Test2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Test2 />
-    // <App />
+    <App />
 )
