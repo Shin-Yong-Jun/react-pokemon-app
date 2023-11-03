67cf700013c3f9eba670865e4d8ec3e2220cc383
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-11-03 22:55:09 +0900

    useRef와 useState와 Let의 변수차이 예제 업데이트 Test1

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Test1 from './Test1.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    // <App />
+    <Test1 />
 )
