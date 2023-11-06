c123b9ab37e8eba375ff09fc9e48b7371440ef84
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-11-06 18:36:43 +0900

    상세페이지 포켓몬상성 모달창으로 구현

--- src/components/DamageRelations.jsx
+++ src/components/DamageRelations.jsx
@@ -4,10 +4,8 @@ import Type from "./Type";
 // 백엔드 데이터 1차 가공!
 // 데이터 받아오고 -> To와 From을 나눠주기 함수 -> 검색한 단어의 데이터를 [key, value] 방식으로 골라주는 함수
 const DamageRelations = ({ damages }) => {
-
   // 지금까지 잘 정돈한 데이터를 저장할 변수 state
   const [damagePokemonForm, setDamagePokemonForm] = useState();
-
 
   useEffect(() => {
     const arrayDamage = damages.map((damage) =>
@@ -20,7 +18,6 @@ const DamageRelations = ({ damages }) => {
       //합치는 부분
       const obj = joinDamageRelations(arrayDamage);
       setDamagePokemonForm(reduceDuplicateValues(postDamageValue(obj.from)));
-
     } else {
       setDamagePokemonForm(postDamageValue(arrayDamage[0].from));
     }
@@ -29,35 +26,29 @@ const DamageRelations = ({ damages }) => {
   const joinDamageRelations = (props) => {
     return {
       to: joinObjects(props, "to"),
-      from: joinObjects(props, 'from'),
+      from: joinObjects(props, "from"),
     };
   };
 
   const reduceDuplicateValues = (props) => {
     const duplicateValues = {
-      double_damage: '4x',
-      half_damage: '1/4x',
-      no_damage: '0x'
-    }
+      double_damage: "4x",
+      half_damage: "1/4x",
+      no_damage: "0x",
+    };
 
-    return Object.entries(props)
-      .reduce((acc, [keyName, value]) => {
-        const key = keyName;
+    return Object.entries(props).reduce((acc, [keyName, value]) => {
+      const key = keyName;
 
-        const verifiedValue = filterForUniqueValues(
-          value,
-          duplicateValues[key]
-        )
-        return (acc = {[keyName]: verifiedValue, ...acc});
-      }, {})
-  }
+      const verifiedValue = filterForUniqueValues(value, duplicateValues[key]);
+      return (acc = { [keyName]: verifiedValue, ...acc });
+    }, {});
+  };
 
   // 먼저 중복되는거 안보이게 하는 함수부터! 4배하기 전에!
   const filterForUniqueValues = (valueForFiltering, damageValue) => {
-
     return valueForFiltering.reduce((acc, currentValue) => {
-
-      //아래가 디스트럭쳐링 쪼개는 코드! 
+      //아래가 디스트럭쳐링 쪼개는 코드!
       const { url, name } = currentValue;
 
       // 중복 안되는 특성만 넣어주기
@@ -65,22 +56,19 @@ const DamageRelations = ({ damages }) => {
 
       // 중복이 없었을 경우
       return filterACC.length === acc.length
-      ? (acc = [currentValue, ...acc])
-      : (acc = [{damageValue: damageValue, name, url}, ...filterACC])
-    }, [])
-  }
-
+        ? (acc = [currentValue, ...acc])
+        : (acc = [{ damageValue: damageValue, name, url }, ...filterACC]);
+    }, []);
+  };
 
   const joinObjects = (props, string) => {
     const key = string;
     const firstArrayValue = props[0][key];
     const secondArrayValue = props[1][key];
 
-
     // 여기가 실제로 합쳐지는 곳
     const result = Object.entries(secondArrayValue).reduce(
       (acc, [keyName, value]) => {
-
         // secondArrayValue의 value를 firstArrayValue에 합친다.
         const result = firstArrayValue[keyName].concat(value);
 
@@ -88,7 +76,7 @@ const DamageRelations = ({ damages }) => {
       },
       {}
     );
-    return result
+    return result;
   };
 
   const postDamageValue = (props) => {
@@ -109,7 +97,7 @@ const DamageRelations = ({ damages }) => {
         ...acc,
       });
     }, {});
-    return result
+    return result;
   };
 
   const seperateObjectBetweenToAndFrom = (damage) => {
@@ -139,45 +127,39 @@ const DamageRelations = ({ damages }) => {
     <div className="flex gap-2 flex-col">
       {damagePokemonForm ? (
         <>
-          {Object.entries(damagePokemonForm)
-            .map(([keyName, value]) => {
-              const key = keyName;
-              const valuesOfKeyName = {
-                double_damage: 'Weak',
-                half_damage: 'Resistant',
-                no_damage: 'Immune'
-              }
+          {Object.entries(damagePokemonForm).map(([keyName, value]) => {
+            const key = keyName;
+            const valuesOfKeyName = {
+              double_damage: "Weak",
+              half_damage: "Resistant",
+              no_damage: "Immune",
+            };
 
-              return(
-                <div key={key}>
-                  <h3 className="capitalize font-medium text-sm md:text-base text-slate-500 text-center">
-                    {valuesOfKeyName[key]}
-                  </h3>
-                  <div className="flex flex-wrap gap-1 justify-center">
-                    {value.length > 0 ? (
-                      value.map(({name, url, damageValue}) => {
-                        return (
-                          <Type
-                            type={name}
-                            key={url}
-                            damageValue={damageValue}
-                          />
-                        )
-                      })
-                    ) : (
-                      <Type type={'none'} key={'none'} />
-
-                    )}
-                  </div>
+            return (
+              <div key={key}>
+                <h3 className="capitalize font-medium text-sm md:text-base text-slate-500 text-center">
+                  {valuesOfKeyName[key]}
+                </h3>
+                <div className="flex flex-wrap gap-1 justify-center mt-1">
+                  {value.length > 0 ? (
+                    value.map(({ name, url, damageValue }) => {
+                      return (
+                        <Type type={name} key={url} damageValue={damageValue} />
+                      );
+                    })
+                  ) : (
+                    <Type type={"none"} key={"none"} />
+                  )}
                 </div>
-              )
-            })}
-        
+              </div>
+            );
+          })}
         </>
-      ) : <div></div>}
+      ) : (
+        <div></div>
+      )}
     </div>
-
-  )
+  );
 };
 
 export default DamageRelations;
--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -10,11 +10,14 @@ import { Vector } from "../../assets/Vector";
 import Type from "../../components/Type";
 import BaseStat from "../../components/BaseStat";
 import DamageRelations from "../../components/DamageRelations";
+import DamageModal from "../../components/DamageModal";
 
 const DetailPage = () => {
   //
   const [pokemon, setPokemon] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  // 모달창
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const params = useParams();
   const pokemonId = params.id;
@@ -184,6 +187,7 @@ const DetailPage = () => {
               loading="lazy"
               alt={pokemon.name}
               className={`object-contain h-full`}
+              onClick={()=>setIsModalOpen(true)}
             />
           </div>
         </section>
@@ -256,7 +260,7 @@ const DetailPage = () => {
           </div>
 
           {/* 데미지 게이지 */}
-          {pokemon.DamageRelations && (
+          {/* {pokemon.DamageRelations && (
             <div className="w-10/12">
               <h2 className={`text-base text-center font-semibold ${text}`}>
                 <DamageRelations 
@@ -267,12 +271,16 @@ const DetailPage = () => {
 
             </div>
 
-          )}
+          )} */}
 
 
 
         </section>
+
       </div>
+            {isModalOpen && <DamageModal 
+              setIsModalOpen={setIsModalOpen}
+              damages={pokemon.DamageRelations}/>}
     </article>
   );
 };
