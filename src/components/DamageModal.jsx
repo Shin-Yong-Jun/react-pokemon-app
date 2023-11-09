874e10e3a2d7354bcd38ad0141b5c673db61c1f7
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-11-09 19:38:20 +0900

    모달창 밖을 클릭할 경우, 모달 닫기 위한 훅스 반영, useOnClickOutside

--- src/components/DamageModal.jsx
+++ src/components/DamageModal.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import DamageRelations from './DamageRelations'
+import useOnClickOutside from '../hooks/useOnClickOutside';
 
 const DamageModal = ({setIsModalOpen, damages }) => {
+
+  const ref = useRef();
+  useOnClickOutside(ref, () => setIsModalOpen(false));
+
   return (
     <div
       className='flex items-center justify-center z-40 fixed left-0 bottom-0 w-full h-full bg-gray-800'
     >
-      <div className='modal bg-white rounded-lg w-1/2'>
+      <div ref={ref} className='modal bg-white rounded-lg w-1/2'>
         <div className='flex flex-col p-4 items-center'>
           <div className='flex items-center w-full justify-between'>
 
