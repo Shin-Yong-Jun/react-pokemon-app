6d98655d559a1590e282cc4f8a93a07acb84543d
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-11-29 14:57:25 +0900

    헤더 nav바 반영 로그인 전

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <NavWrapper>
+      <Logo>
+        <Image 
+          alt = "Poke Logo"
+          src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+          onClick = {() => (window.location.href = "/")}
+          />
+      </Logo>
+    </NavWrapper>
   )
 }
 
-const NavWrapper = () = 
+const Image = styled.img`
+  cursor: pointer;
+  width: 100%;
+  
+`;
+
+
+const Logo = styled.a`
+  padding: 0;
+  width: 50px;
+  margin-top: 4px;
+
+  img {
+    width: 100%;
+  }
+`
+
+const NavWrapper = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 100;
+  `
 
 export default NavBar 
