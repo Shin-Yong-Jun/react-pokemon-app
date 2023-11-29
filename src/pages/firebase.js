982d3bffd5da3e1e84ecb807e14bcfd7d9ba0a6d
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-11-29 12:06:30 +0900

    로그인페이지 반영 및 중첩라우터구조 반영

---
+++ src/pages/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYsfJ2Yscd7ort8ARyjsK3PeJSP83G_Kg",
+  authDomain: "react-poke-app-2529e.firebaseapp.com",
+  projectId: "react-poke-app-2529e",
+  storageBucket: "react-poke-app-2529e.appspot.com",
+  messagingSenderId: "48102362873",
+  appId: "1:48102362873:web:e7e458d009e8a3387e9c28"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
