1cbe3d56de4d68e32bb26a5657aef5fda6c2839d
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2024-01-30 09:14:53 +0900

    콜론 마무리

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from 'axios';
 import PokeCard from "../../components/PokeCard";
 import AutoComplete from "../../components/AutoComplete";
 import { PokemonData, PokemonNameAndUrl } from "../../types/PokemonData";
